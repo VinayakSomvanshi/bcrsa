0bc2c71d3d5a55bb3269bd31aac20def30582f3c
GreatMidoriya <91780961+GreatMidoriya@users.noreply.github.com>
2024-03-18 20:08:57 +0530

    first commit

--- forveel/lib/main.dart
+++ forveel/lib/main.dart
@@ -1,22 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:forveel/ui/Mycolors.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
 import 'package:forveel/ui/Widgets/Start/logindecider.dart';
+import 'package:permission_handler/permission_handler.dart'; // Add this line
+
+import 'firebase_options.dart';
 
 void main() async {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      systemNavigationBarColor: Colors.transparent));
-       WidgetsFlutterBinding.ensureInitialized();
+    statusBarColor: Colors.transparent,
+    systemNavigationBarColor: Colors.transparent,
+  ));
+  WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Request location permission here
+  await _requestLocationPermission();
+
   runApp(MaterialApp(
-    theme: new ThemeData(
+    theme: ThemeData(
       backgroundColor: background,
       hintColor: textc,
       buttonColor: green,
@@ -27,4 +33,14 @@ void main() async {
     title: "RSA",
     home: LoginDecider(),
   ));
+}
+
+// Function to request location permission
+Future<void> _requestLocationPermission() async {
+  PermissionStatus status = await Permission.location.request();
+  if (status.isGranted) {
+    print('Location permission granted.');
+  } else {
+    print('Location permission denied.');
+  }
 }
